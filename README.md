78c1bbb5966ecbaebd18a6c4a96f78047d2c592a
Hilary Mallar <hilarymallar@gmail.com>
2018-10-21 16:25:56 -0400

    cleaned up and added background

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Train-Scheduler
+
+*Can add a train to the schedule using Add Train with a start time and frequency.
+*Stores the trains in the Firebase database.
+*The train gets put in the schedule with the next arrival time and the number of minutes until the next train using MomentJS.  
+*Upon initial load of the page, and everytime the a train is added to the database, the Train Schedule is updated.
+*Can delete the trains from the schedule and the database, using the Delete button.
+*Design created using Bootstrap.
---
+++ assets/javascript/app.js
@@ -0,0 +1,98 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAKN45koWKKv0ZR-8ioPHfk7suHU1RVMtM",
+    authDomain: "train-time-schedule-a8788.firebaseapp.com",
+    databaseURL: "https://train-time-schedule-a8788.firebaseio.com",
+    projectId: "train-time-schedule-a8788",
+    storageBucket: "train-time-schedule-a8788.appspot.com",
+    messagingSenderId: "745238277439"
+};
+firebase.initializeApp(config);
+
+//When adding a train, add it to the database
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+
+    //get the input values
+    var trainName = $("#train-name-input").val().trim();
+    var dest = $("#destination-input").val().trim();
+    var first = $("#first-input").val().trim();
+    var freq = $("#frequency-input").val().trim();
+
+    //push the value to the firebase database
+    firebase.database().ref().push({
+        trainName: trainName,
+        destination: dest,
+        first: first,
+        frequency: freq
+    });
+
+    //clear input values
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-input").val("");
+    $("#frequency-input").val("");
+
+});
+
+// At the initial load and subsequent value changes, get a snapshot of the stored data.
+// This function allows you to update your page in real-time when the firebase database changes.
+firebase.database().ref().on("child_added", function (snapshot) {
+
+    //create a new row and the corresponding html
+    var newRow = $("<tr>");
+    var colName = $("<td scope='col'>").text(snapshot.val().trainName);
+    newRow.append(colName);
+    var colDest = $("<td scope='col'>").text(snapshot.val().destination);
+    newRow.append(colDest);
+    var colFreq = $("<td scope='col'>").text(snapshot.val().frequency);
+    newRow.append(colFreq);
+
+    //Perform calculations for the remaining rows
+    // Get the frequency and the first train time
+    var tFrequency = snapshot.val().frequency;
+
+    // First time
+    var firstTime = snapshot.val().first;
+
+    // First Time (pushed back 1 day to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "day");
+    
+    // Difference between the times
+    var diffTime = moment().diff(firstTimeConverted, "minutes");
+   
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    nextTrain = nextTrain.format("LT");
+ 
+    var next = $("<td scope='col'>").text(nextTrain);
+    newRow.append(next);
+    var mins = $("<td scope='col'>").text(tMinutesTillTrain);
+    newRow.append(mins);
+
+    //add a button to delete a train, use the snapshot key as an attribute
+    var button = $("<td scope='col'>");
+    button.append($("<button class='btn float-left blue font-weight-bold' id='delete-train-btn'>").attr("key", snapshot.key).text("Delete"));
+    newRow.append(button);
+
+    //add the row to the table body
+    $("tbody").append(newRow);
+
+    // If any errors are experienced, log them to console.
+}, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+});
+
+//If a delete button is clicked, delete the value from the database and delete the row from the table.
+$(document).on("click", "#delete-train-btn", function () {
+    var key = $(this).attr("key");
+    firebase.database().ref().child(key).remove();
+    var row = $(this).parent().parent();
+    row.remove();
+});
